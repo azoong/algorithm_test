aa7a4cc2c01b24f46f1a4d1d3e39a95168d7f9a8
azoong <shshinkitec2@gmail.com>
2023-03-03 15:08:26 +0900

    할인행사

---
+++ 할인 행사.js
@@ -0,0 +1,33 @@
+//문제 출처 https://school.programmers.co.kr/learn/courses/30/lessons/131127
+
+/*문제 설명
+
+출처 참고
+
+*/
+
+const want = ["banana", "apple", "rice", "pork", "pot"]
+const number = [3, 2, 2, 2, 1]
+const discount = ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"]
+
+
+function solution(want, number, discount) {
+    let answer = 0;
+    const isMatch = (discount) => {
+      const wantMap = new Map();
+      discount.forEach((d) => wantMap.set(d, (wantMap.get(d) || 0) + 1));
+      for (let i = 0; i < want.length; i++) {
+        // 총개수가 10개로 정해져있기 때문에 값이 크더라도 탈락시킵니다
+        if (wantMap.get(want[i]) !== number[i]) return false;
+      }
+      return true;
+      
+    };
+    for (let idx = 0; idx <= discount.length - 10; idx++) {
+        const d = discount.slice(idx, idx + 10);
+        if (isMatch(d)) answer++;
+      }
+      return answer;
+}
+
+console.log(solution(want,number,discount))
