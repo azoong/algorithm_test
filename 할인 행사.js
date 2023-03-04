a93ca490a83da20083cc1368213f844ec8586d69
azoong <shshinkitec2@gmail.com>
2023-03-04 22:58:41 +0900

    할인행사 풀이

--- 할인 행사.js
+++ 할인 행사.js
@@ -11,23 +11,50 @@ const number = [3, 2, 2, 2, 1]
 const discount = ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"]
 
 
+
+
 function solution(want, number, discount) {
-    let answer = 0;
-    const isMatch = (discount) => {
-      const wantMap = new Map();
-      discount.forEach((d) => wantMap.set(d, (wantMap.get(d) || 0) + 1));
-      for (let i = 0; i < want.length; i++) {
-        // 총개수가 10개로 정해져있기 때문에 값이 크더라도 탈락시킵니다
-        if (wantMap.get(want[i]) !== number[i]) return false;
+  let answer = 0
+  const wantMap = new Map()
+  // 필요한 물품 개수 정리
+  for (let i = 0; i<want.length; i++){
+      wantMap.set(want[i], number[i])
+  }
+  // 할인 품목 개수 확인후 비교.
+  loop : for (let i = 0; i <= discount.length - 10; i++){
+      let slice = discount.slice(i,i+10)
+      let discountMap = new Map()
+      slice.forEach((item) => discountMap.set(item, (discountMap.get(item)||0) +1))
+      //할인 품목 객체를 만들어준다.
+      //객체의 값을 비교
+      for(let j = 0; j<want.length; j++){
+          if(wantMap.get(want[j]) !== (discountMap.get(want[j])||0)) continue loop;
       }
-      return true;
-      
-    };
-    for (let idx = 0; idx <= discount.length - 10; idx++) {
-        const d = discount.slice(idx, idx + 10);
-        if (isMatch(d)) answer++;
-      }
-      return answer;
+      answer ++
+  }
+
+  return answer;
 }
+
+
+// function solution(want, number, discount) {
+//     let answer = 0;
+//     const isMatch = (discount) => {
+//       const wantMap = new Map();
+//       discount.forEach((d) => wantMap.set(d, (wantMap.get(d) || 0) + 1));
+//       for (let i = 0; i < want.length; i++) {
+//         // 총개수가 10개로 정해져있기 때문에 값이 크더라도 탈락시킵니다
+//         if (wantMap.get(want[i]) !== number[i]) return false;
+//       }
+//       return true;
+      
+//     };
+//     for (let idx = 0; idx <= discount.length - 10; idx++) {
+//         const d = discount.slice(idx, idx + 10);
+//         if (isMatch(d)) answer++;
+//       }
+//       return answer;
+// }
+
 
 console.log(solution(want,number,discount))
